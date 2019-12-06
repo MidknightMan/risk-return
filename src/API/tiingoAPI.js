a81531c459c4f9ec152512e3cd2ba0fdb6fd77d2
New User <newuser@news-mbp.mynet>
2019-12-06 08:51:38 +0000

    proxies the tiingo API search for securities by name

--- src/API/tiingoAPI.js
+++ src/API/tiingoAPI.js
@@ -4,7 +4,7 @@ import tiingoKey from '../keys';
 export const getCompanyList = query => {
   return axios
     .get(
-      `https://api.tiingo.com/tiingo/utilities/search?query=${query}&token=${tiingoKey}`
+      `https://proxy-server.herokuapp.com/https://api.tiingo.com/tiingo/utilities/search?query=${query}&token=${tiingoKey}`
     )
     .then(({ data }) => {
       return data;
